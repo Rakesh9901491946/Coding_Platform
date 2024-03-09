f4b853e7c9cb251c89411e2d308411396221d9b3
Rakesh N J <42140694+Rakesh9901491946@users.noreply.github.com>
2024-03-09 18:12:43 +0100

    Update test.py

--- test.py
+++ test.py
@@ -2,6 +2,6 @@ from flask import Flask, render_template, redirect, url_for, request
 
 app = Flask(__name__)
 @app.route('/coding')
-def dashboard2():
+def coding():
    
     return render_template('coding.html')
